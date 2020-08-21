7942c9e09e3745b0c2f8a552e78644a7fd9aa25a
mmorphew <68831473+mmorphew@users.noreply.github.com>
2020-08-21 11:23:02 -0500

    Updated app - a slider can now be used to pan through remote sensing images

--- app.py
+++ app.py
@@ -3,32 +3,36 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
+import glob
+import os
 
 app = dash.Dash()
 server = app.server
-
+file_list = glob.glob(r'.\assets\*')
+for i in range(len(file_list)):
+    file_list[i] = os.path.basename(file_list[i])
 app.layout = html.Div([
-
     html.Section(id="slideshow", children=[
         html.Div(id="slideshow-container", children=[
             html.Div(id="image"),
             dcc.Interval(id='interval', interval=3000)
-        ])
+        ]),
+    dcc.Slider(
+        id='my-slider',
+        min=0, max=len(file_list)-1, step=1, value=10),
+    html.Div(id='slider-output-container'),
     ])
 
 ])
-
+@app.callback(
+    dash.dependencies.Output('slider-output-container', 'children'),
+    [dash.dependencies.Input('my-slider', 'value')])
+def update_output(value):
+    return '{}'.format(file_list[value])
 @app.callback(Output('image', 'children'),
-              [Input('interval', 'n_intervals')])
-def display_image(n):
-    if n == None or n % 3 == 1:
-        img = html.Img(src="https://www.mail-signatures.com/wp-content/uploads/2019/02/How-to-find-direct-link-to-image_Blog-Picture.png")
-    elif n % 3 == 2:
-        img = html.Img(src="http://placeimg.com/625/225/animals")
-    elif n % 3 == 0:
-        img = html.Img(src="http://placeimg.com/625/225/arch")
-    else:
-        img = "None"
+              [Input('my-slider', 'value')])
+def display_image(value):
+    img = html.Img(src= app.get_asset_url(file_list[value]), height=512, width=512)
     return img
 
 if __name__ == '__main__':
