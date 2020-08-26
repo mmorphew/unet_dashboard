42e16f84b97eb10acdb1ea1a95da76b193096842
mmorphew <68831473+mmorphew@users.noreply.github.com>
2020-08-26 10:51:16 -0500

    Updated image interface, slider now updates on drag rather than mouse release

--- app.py
+++ app.py
@@ -3,37 +3,63 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+import plotly.express as px
 import glob
 import os
+import cv2
+import json
+import pandas as pd
+from skimage.transform import resize
 
-app = dash.Dash()
-server = app.server
 file_list = glob.glob(r'.\assets\*')
-for i in range(len(file_list)):
-    file_list[i] = os.path.basename(file_list[i])
+image = plt.imread(file_list[0])
+image = resize(image, (256, 256, 3)) # resize it
+# Getting the kernel to be used in Top-Hat
+filterSize = 400
+filterSize =(filterSize, filterSize) 
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT,  
+                                   filterSize) 
+  
+# Applying the Top-Hat operation 
+tophat_img = cv2.morphologyEx(image,  
+                              cv2.MORPH_TOPHAT, 
+                              kernel) 
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+
+fig = px.imshow(image)
+
+fig.update_layout(clickmode='event+select')
+
 app.layout = html.Div([
-    html.Section(id="slideshow", children=[
-        html.Div(id="slideshow-container", children=[
-            html.Div(id="image"),
-            dcc.Interval(id='interval', interval=3000)
-        ]),
+    dcc.Graph(
+        id='image'),
     dcc.Slider(
         id='my-slider',
-        min=0, max=len(file_list)-1, step=1, value=10),
-    html.Div(id='slider-output-container'),
-    ])
-
+        min=0, max=len(file_list)-1, step=1, value=int(len(file_list)/2),
+        updatemode='drag'),
+    html.Div(id='slider-output-container')
 ])
-@app.callback(
-    dash.dependencies.Output('slider-output-container', 'children'),
-    [dash.dependencies.Input('my-slider', 'value')])
-def update_output(value):
-    return '{}'.format(file_list[value])
-@app.callback(Output('image', 'children'),
+
+@app.callback(Output('image', 'figure'),
               [Input('my-slider', 'value')])
-def display_image(value):
-    img = html.Img(src= app.get_asset_url(file_list[value]), height=512, width=512)
-    return img
+def update_figure(image_choice):
+    image = plt.imread(file_list[image_choice])
+    image = resize(image, (256, 256, 3))
+    fig = px.imshow(image)
+    fig.update_layout(clickmode='event+select')
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
